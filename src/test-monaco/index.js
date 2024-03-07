0b781eef236225c566cae3bc81592f514f6f54a1
hexiang <hexiang@cestc.cn>
2024-03-07 23:10:48 +0800

    fix: 引入monaco-editor-webpack-plugin

--- src/test-monaco/index.js
+++ src/test-monaco/index.js
@@ -2,24 +2,27 @@ import React, { useEffect, useRef } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 import { createRoot } from "react-dom/client"
 import * as monaco from "monaco-editor"
+import metadata from 'monaco-editor/esm/metadata';
 
-self.MonacoEnvironment = {
-    getWorkerUrl: function (moduleId, label) {
-        if (label === 'json') {
-            return './json.worker.bundle.js';
-        }
-        if (label === 'css' || label === 'scss' || label === 'less') {
-            return './css.worker.bundle.js';
-        }
-        if (label === 'html' || label === 'handlebars' || label === 'razor') {
-            return './html.worker.bundle.js';
-        }
-        if (label === 'typescript' || label === 'javascript') {
-            return './ts.worker.bundle.js';
-        }
-        return './editor.worker.bundle.js';
-    }
-};
+console.log("metadata.languages:", metadata)
+
+// self.MonacoEnvironment = {
+//     getWorkerUrl: function (moduleId, label) {
+//         if (label === 'json') {
+//             return './json.worker.bundle.js';
+//         }
+//         if (label === 'css' || label === 'scss' || label === 'less') {
+//             return './css.worker.bundle.js';
+//         }
+//         if (label === 'html' || label === 'handlebars' || label === 'razor') {
+//             return './html.worker.bundle.js';
+//         }
+//         if (label === 'typescript' || label === 'javascript') {
+//             return './ts.worker.bundle.js';
+//         }
+//         return './editor.worker.bundle.js';
+//     }
+// };
 
 export function App() {
 
--- src/test-monaco/webpack.config.js
+++ src/test-monaco/webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: {
         app: './index.js',
-        'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
-        'json.worker': 'monaco-editor/esm/vs/language/json/json.worker',
-        'css.worker': 'monaco-editor/esm/vs/language/css/css.worker',
-        'html.worker': 'monaco-editor/esm/vs/language/html/html.worker',
-        'ts.worker': 'monaco-editor/esm/vs/language/typescript/ts.worker'
+        // 'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
+        // 'json.worker': 'monaco-editor/esm/vs/language/json/json.worker',
+        // 'css.worker': 'monaco-editor/esm/vs/language/css/css.worker',
+        // 'html.worker': 'monaco-editor/esm/vs/language/html/html.worker',
+        // 'ts.worker': 'monaco-editor/esm/vs/language/typescript/ts.worker'
     },
     output: {
         globalObject: 'self',
@@ -35,6 +36,10 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
+        }),
+        new MonacoWebpackPlugin({
+            // available options are documented at https://github.com/microsoft/monaco-editor/blob/main/webpack-plugin/README.md#options
+            languages: ['json', 'javascript', 'typescript']
         })
     ]
 };
