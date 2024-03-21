3390e3e045b68753d99ace488c4777665f3642aa
hexiang <hexiang@cestc.cn>
2024-03-21 18:10:41 +0800

    feat: 测试lodash merge

--- src/app2/index.js
+++ src/app2/index.js
@@ -2,12 +2,19 @@ import { createRoot } from "react-dom/client";
 import * as React from "react";
 import "../util";
 import "./index.css";
+import * as _ from "lodash";
+import dest from "./dest.json";
+import source from "./source.json";
+
+const res = _.merge(dest, source);
+debugger;
 
 function App() {
   return (
     <>
       <div className="mark"></div>
       <div className="border-image">app2</div>
+      <em-emoji id="grinning" />
     </>
   );
 }
