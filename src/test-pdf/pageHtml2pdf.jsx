abf4dc353a2145436f7e1efc1e5d0450a1656947
hexiang <hexiang@cestc.cn>
2024-05-07 19:31:16 +0800

    feat:  添加x6

--- src/test-pdf/pageHtml2pdf.jsx
+++ src/test-pdf/pageHtml2pdf.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import html2pdf from "html2pdf.js";
+import { Input, Modal } from "@arco-design/web-react";
+import '@arco-themes/react-ocean-design/css/arco.css';
 
 function App() {
   const iframeRef = React.useRef();
+  const [visible,setVisible]=useState(false)
   React.useEffect(() => {}, []);
   return (
     <div>
-      <div ref={iframeRef}>下载</div>
+      <div>下载</div>
       <button
         onClick={() => {
-          html2pdf(document.body);
+          setVisible(true)
         }}
       >
         开始
       </button>
+      <Modal  visible={visible} onCancel={()=>setVisible(false)} onOk={()=>{
+        html2pdf(iframeRef.current,{margin:20});
+      }}>
+        <div ref={iframeRef} style={{height:'500px',overflow:'auto',position:'relative'}}>
+          <Input style={{position:'absolute',top:0,left:0}} placeholder="hello"/>
+        {Array(100).fill(0).map(()=>{
+          return <div>hello</div>
+        })}
+        </div>
+      </Modal>
     </div>
   );
 }
