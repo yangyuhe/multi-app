f6b75ac659c28e42a0c9dd1eecf11506838de176
hexiang <571536582@qq.com>
2024-06-25 08:28:53 +0800

    feat: whistle.myplugin

--- src/whistle.myplugin/src/rulesServer.ts
+++ src/whistle.myplugin/src/rulesServer.ts
@@ -7,7 +7,10 @@ export default (
     "request",
     (req: Whistle.PluginRequest, res: Whistle.PluginResponse) => {
       console.log("rulesServer process");
-      res.end();
+
+      res.end(
+        "^***/api/plugins/console-plugin-appforge/** http://127.0.0.1:9002/$2"
+      );
     }
   );
 };
