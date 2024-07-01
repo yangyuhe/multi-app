2e43391426f155c8e4f89e72dbe253c34f3d77d4
hexiang <571536582@qq.com>
2024-07-02 00:05:36 +0800

    feat: whistle

--- src/whistle.myplugin/src/server.ts
+++ src/whistle.myplugin/src/server.ts
@@ -12,7 +12,78 @@ export default (
       console.log("server process");
 
       //处理
-      if (false) {
+      let url = new URL(req.fullUrl);
+      const consoleConfig: ConsoleConfig = require("../public/consoleConfig.json");
+      const matchedConsoleConfig = consoleConfig.filter(
+        (i) => i.domain === "*" || i.domain === url.hostname
+      );
+      if (url.pathname === "/console/api/plugins/plugin-manifests.json") {
+        const client = req.request((svrRes) => {
+          const gunzipped = zlib.createGunzip();
+          let body = [];
+          gunzipped.on("data", (chunk) => {
+            body.push(chunk.toString());
+          });
+
+          gunzipped.on("end", async () => {
+            console.log("解压完成。");
+            svrRes.headers["content-encoding"] = "identify";
+            console.log(svrRes.headers);
+            res.writeHead(svrRes.statusCode, svrRes.headers);
+            const oldData = JSON.parse(body.join(""));
+
+            for (let item of matchedConsoleConfig) {
+              for (let plugin of item.plugin) {
+                const res = await fetch(plugin.url);
+                const body = await res.body;
+                const index = oldData.findIndex((i) => i.name === plugin.name);
+                oldData.splice(index, 1, JSON.parse(body));
+              }
+            }
+
+            res.end(JSON.stringify(oldData));
+          });
+          svrRes.pipe(gunzipped);
+
+          gunzipped.on("error", (err) => {
+            console.error("解压错误:", err);
+          });
+        });
+        req.pipe(client);
+      } else if (
+        url.hostname.endsWith("cecloud.com") &&
+        req.headers["accept"]?.includes("text/html")
+      ) {
+        if (matchedConsoleConfig.length > 0) {
+          const client = req.request((svrRes) => {
+            let body = [];
+            svrRes.on("data", (chunk) => {
+              body.push(chunk.toString());
+            });
+
+            svrRes.on("end", async () => {
+              let html = body.join("");
+              const regRes = html.match(/"consolePlugins":\[([^\]]*)\]/);
+              if (regRes) {
+                const plugins = regRes[1].split(",");
+                const newAddPlugins = [];
+                for (let domain of matchedConsoleConfig) {
+                  for (let plugin of domain.plugin) {
+                    if (!plugins.includes(plugin.name))
+                      newAddPlugins.push(plugin.name);
+                  }
+                }
+                if (newAddPlugins.length > 0) {
+                  html = html
+                    .split(regRes[1])
+                    .join(plugins.concat(newAddPlugins).join(","));
+                }
+              }
+              res.end(html);
+            });
+          });
+          req.pipe(client);
+        }
       } else {
         req.passThrough();
       }
