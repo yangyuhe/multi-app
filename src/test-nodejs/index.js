b6576c48136360cc2fa8f639d54c91924b84f589
hexiang <hexiang@cestc.cn>
2024-08-16 11:31:33 +0800

    feat: 测试git大小写和ipv6

--- src/test-nodejs/index.js
+++ src/test-nodejs/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   res.write("hello");
   res.end();
 });
-server.listen("9008");
+server.listen(9008, "::1");
