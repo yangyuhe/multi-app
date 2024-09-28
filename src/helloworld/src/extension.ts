887a8f81f8d5f5fe2ee252e4ad10a9075707a0d2
hexiang <571536582@qq.com>
2024-09-29 00:08:06 +0800

    feat: 修改vscode插件

--- src/helloworld/src/extension.ts
+++ src/helloworld/src/extension.ts
@@ -23,4 +23,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log("deactivate")
+}
